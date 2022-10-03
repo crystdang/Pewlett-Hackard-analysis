c2b8e2a32fb0b4a20936ee9b3f477738ec3e3d2f
crystdang <111620460+crystdang@users.noreply.github.com>
2022-10-03 17:29:43 -0400

    correctly uploaded tables

--- schema.sql
+++ schema.sql
@@ -38,7 +38,7 @@ CREATE TABLE dept_emp (
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-  PRIMARY KEY (dept_no)
+  PRIMARY KEY (emp_no, dept_no)
 	
 );
 CREATE TABLE titles (
@@ -47,7 +47,8 @@ CREATE TABLE titles (
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (title)
+  PRIMARY KEY (emp_no, title, from_date)
 	
 );
 SELECT * FROM departments;
+DROP TABLE titles CASCADE;
